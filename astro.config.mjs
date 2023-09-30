2e87b700ba86602f618168c063d26a30c1892719
Grégory Alary <gregalary26@gmail.com>
2023-09-30 17:11:16 +0200

    feat: deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   compressHTML: true,
+  site: 'https://carte-du-patrimoine.gregoryalary.dev',
 });
