0fb827e453cd021a7e9974e89e3397bda6be6538
Grégory Alary <gregalary26@gmail.com>
2024-07-22 10:06:01 +0200

    feat: redirect qr

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,8 @@ export default defineConfig({
   compressHTML: true,
   site: 'https://www.carte-du-patrimoine.com',
   redirects: {
+    '/qr/': 'https://mh-map-api.gregoryalary.xyz/api/qr',
+    '/qr-2/': 'https://mh-map-api.gregoryalary.xyz/api/qr',
     '/blog/': '/blog/1/',
     '/blog/panneaux-monuments-historiques-tout-ce-qu-il-faut-savoir/': '/blog/tout-savoir-sur-les-panneaux-monument-historique/'
   },
